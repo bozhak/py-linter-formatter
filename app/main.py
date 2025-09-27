84c4b4cf6314e9807b0e2cec28bf8f135be87ac0
Oleh Bozhak <olbozhak@gmail.com>
2025-09-27 15:46:34 +0300

    Solution, try #2

--- app/main.py
+++ app/main.py
@@ -23,29 +23,34 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
                     } for i in errors
                 ],
             "path": file_path,
-            "status": "failed"
+            "status": "passed" if not errors else "failed"
         }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": linter_report[list(linter_report.keys())[0]],
-            "path": list(linter_report.keys())[0],
-            "status": "passed"
-        },
-    ] + [
-        { "errors":
+    return \
+        (
             [
                 {
-                    "line": result["line_number"],
-                    "column": result["column_number"],
-                    "message": result["text"],
-                    "name": result["code"],
-                    "source": "flake8"
-                } for result in value
-            ],
-            "path": key,
-            "status": "failed"
-        } for key, value in list(linter_report.items())[1:]
-    ]
+                    "errors": linter_report[list(linter_report.keys())[0]],
+                    "path": list(linter_report.keys())[0],
+                    "status": "passed"
+                },
+            ] +
+            [
+                {
+                    "errors":
+                        [
+                            {
+                                "line": result["line_number"],
+                                "column": result["column_number"],
+                                "message": result["text"],
+                                "name": result["code"],
+                                "source": "flake8"
+                            } for result in value
+                        ],
+                    "path": key,
+                    "status": "passed" if not linter_report else "failed"
+                } for key, value in list(linter_report.items())[1:]
+            ]
+        )
