efc8bb97ae2f2e814339222afea3376adea34df5
Oleh Bozhak <olbozhak@gmail.com>
2025-09-27 14:57:37 +0300

    solution #1

--- app/main.py
+++ app/main.py
@@ -1,13 +1,51 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return \
+        {
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+        }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return \
+        {
+            "errors":
+                [
+                    {
+                        "line": i["line_number"],
+                        "column": i["column_number"],
+                        "message": i["text"],
+                        "name": i["code"],
+                        "source": "flake8"
+                    } for i in errors
+                ],
+            "path": file_path,
+            "status": "failed"
+        }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": linter_report[list(linter_report.keys())[0]],
+            "path": list(linter_report.keys())[0],
+            "status": "passed"
+        },
+    ] + [
+        { "errors":
+            [
+                {
+                    "line": result["line_number"],
+                    "column": result["column_number"],
+                    "message": result["text"],
+                    "name": result["code"],
+                    "source": "flake8"
+                } for result in value
+            ],
+            "path": key,
+            "status": "failed"
+        } for key, value in list(linter_report.items())[1:]
+    ]
